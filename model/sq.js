96f84af6fba72efda4dac57ab08e970cb89b8f55
chiya22 <tomoyay@gmail.com>
2020-09-08 16:27:08 +0900

    knex対応

--- model/sq.js
+++ model/sq.js
@@ -1,23 +1,17 @@
-const connection = require('../db/mysqlconfig.js');
 const knex = require("../db/knex.js");
 
 const getSqCompany = function (callback) {
     (async function () {
+
         const query1 = 'update sq_company set id=LAST_INSERT_ID(id+1)';
         const query2 = 'select LAST_INSERT_ID() as no from sq_company';
-        connection.query(query1, function (error, results, fields) {
-            if (error) {
-                callback(error, null);
-            } else {
-                connection.query(query2, function (error, results, fields) {
-                    if (error) {
-                        callback(error, null);
-                    } else {
-                        callback(null, results[0]);
-                    }
-                });
-            }
-        });
+
+        const client = knex.connect();
+        const retObj1 = await client.raw(query1);
+        const retObj2 = await client.raw(query2);
+
+        callback(null, retObj2[0][0]);
+
     })();
 };
 
@@ -25,19 +19,11 @@ const getSqPerson = function (callback) {
     (async function () {
         const query1 = 'update sq_person set id=LAST_INSERT_ID(id+1)';
         const query2 = 'select LAST_INSERT_ID() as no from sq_person';
-        connection.query(query1, function (error, results, fields) {
-            if (error) {
-                callback(error, null);
-            } else {
-                connection.query(query2, function (error, results, fields) {
-                    if (error) {
-                        callback(error, null);
-                    } else {
-                        callback(null, results[0]);
-                    }
-                });
-            }
-        });
+        const client = knex.connect();
+        const retObj1 = await client.raw(query1);
+        const retObj2 = await client.raw(query2);
+
+        callback(null, retObj2[0][0]);
     })();
 };
 
@@ -45,19 +31,11 @@ const getSqOutai = function (callback) {
     (async function () {
         const query1 = 'update sq_outai set id=LAST_INSERT_ID(id+1)';
         const query2 = 'select LAST_INSERT_ID() as no from sq_outai';
-        connection.query(query1, function (error, results, fields) {
-            if (error) {
-                callback(error, null);
-            } else {
-                connection.query(query2, function (error, results, fields) {
-                    if (error) {
-                        callback(error, null);
-                    } else {
-                        callback(null, results[0]);
-                    }
-                });
-            }
-        });
+        const client = knex.connect();
+        const retObj1 = await client.raw(query1);
+        const retObj2 = await client.raw(query2);
+
+        callback(null, retObj2[0][0]);
     })();
 };
 
