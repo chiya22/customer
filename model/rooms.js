562f90840942d662e93977f36adefd6c59d16364
chiya22 <tomoyay@gmail.com>
2020-09-10 10:18:07 +0900

    knex対応

--- model/rooms.js
+++ model/rooms.js
@@ -1,79 +1,85 @@
-var connection = require('../db/mysqlconfig.js');
 const tool = require('../util/tool');
+const knex = require("../db/knex.js");
 
 const findPKey = function (pkey, callback) {
     (async function () {
-        await connection.query('select * from rooms where id = "' + pkey + '" and ymd_end = "99991231" order by id asc', function (error, results, fields) {
-            if (error) {
-                callback(error, null);
-            } else {
-                callback(null, results[0]);
-            }
-        });
+        const client = knex.connect();
+        await client.from("rooms").where({ id: pkey })
+        .then( (retObj) => {
+            callback(null, retObj[0]);
+        })
+        .catch( (err) => {
+            callback(err, null);
+        })
     })();
 };
 
 const find = function (callback) {
     (async function () {
-        await connection.query('select * from rooms where ymd_end = "99991231" order by place asc, floor asc', function (error, results, fields) {
-            if (error) {
-                callback(error, null);
-            } else {
-                callback(null, results);
-            }
-        });
+        const client = knex.connect();
+        await client.from("rooms").orderBy({place: "asc", floor: "asc"})
+        .then( (retObj) => {
+            callback(null, retObj[0]);
+        })
+        .catch( (err) => {
+            callback(err, null);
+        })
     })();
 };
 
 const findForAdmin = function (callback) {
     (async function () {
+        const client = knex.connect();
         const query = 'SELECT r.*,  c.name as companyname from rooms r left outer join relation_comroom re ON r.id = re.id_room AND r.ymd_end = "99991231" AND re.ymd_end = "99991231"  left outer join companies c ON re.id_company = c.id order BY r.place ASC, r.floor asc'
-        await connection.query(query, function (error, results, fields) {
-            if (error) {
-                callback(error, null);
-            } else {
-                callback(null, results);
-            }
-        });
+        await client.raw(query)
+        .then( (retObj) => {
+            callback(null, retObj[0]);
+        })
+        .catch( (err) => {
+            callback(err, null);
+        })
     })();
 };
 
 const insert = function (inObj, callback) {
     (async function () {
-        const query = 'insert into rooms values (' + tool.returnvalue(inObj.id) + ',' + tool.returnvalue(inObj.place) + ',' + tool.returnvalue(inObj.floor) + ',' + tool.returnvalue(inObj.name) + ', "' + inObj.ymd_start + '", "99991231", "' + inObj.ymd_upd + '", "' + inObj.id_upd + '")';
-        connection.query(query, function (error, results, fields) {
-            if (error) {
-                callback(error, null);
-            } else {
-                callback(null, results);
-            }
-        });
+        const client = knex.connect();
+        const query = 'insert into rooms values (' + tool.returnvalue(inObj.id) + ',' + tool.returnvalue(inObj.place) + ',' + tool.returnvalue(inObj.floor) + ',' + tool.returnvalue(inObj.person) + ',' + tool.returnvalue(inObj.name) + ', "' + inObj.ymd_start + '", "99991231", "' + inObj.ymd_upd + '", "' + inObj.id_upd + '")';
+        await client.raw(query)
+        .then( (retObj) => {
+            callback(null, retObj[0]);
+        })
+        .catch( (err) => {
+            callback(err, null);
+        })
     })();
 };
 
 const update = function (inObj, callback) {
     (async function () {
-        const query = 'update rooms set place = ' + tool.returnvalue(inObj.place) + ', floor = ' + tool.returnvalue(inObj.floor) + ', name = ' + tool.returnvalue(inObj.name) + ', ymd_upd = "' + inObj.ymd_upd + '", id_upd = "' + inObj.id_upd + '" where id = ' + tool.returnvalue(inObj.id) + ' and ymd_end = "99991231"';
-        connection.query(query, function (error, results, fields) {
-            if (error) {
-                callback(error, null);
-            } else {
-                callback(null, results);
-            }
-        });
+        const client = knex.connect();
+        const query = 'update rooms set place = ' + tool.returnvalue(inObj.place) + ', floor = ' + tool.returnvalue(inObj.floor) + ', person = ' + inObj.person + ', name = ' + tool.returnvalue(inObj.name) + ', ymd_start = "' + inObj.ymd_start + '", ymd_end = "' + inObj.ymd_end + '", ymd_upd = "' + inObj.ymd_upd + '", id_upd = "' + inObj.id_upd + '" where id = ' + tool.returnvalue(inObj.id) + ' and ymd_end = "' + inObj.before_ymd_end + '"';
+        await client.raw(query)
+        .then( (retObj) => {
+            callback(null, retObj[0]);
+        })
+        .catch( (err) => {
+            callback(err, null);
+        })
     })();
 };
 
 const remove = function (pkey, callback) {
     (async function () {
-        const query = 'delete from rooms where id = "' + pkey + '" and ymd_end = "99991231"';
-        connection.query(query, function (error, results, fields) {
-            if (error) {
-                callback(error, null);
-            } else {
-                callback(null, results);
-            }
-        });
+        const client = knex.connect();
+        const query = 'delete from rooms where id = "' + pkey + '"';
+        await client.raw(query)
+        .then( (retObj) => {
+            callback(null, retObj[0]);
+        })
+        .catch( (err) => {
+            callback(err, null);
+        })
     })();
 };
 
