5b49b2a89cb4d1ee96041adc449e97e20a2e880a
chiya22 <tomoyay@gmail.com>
2021-01-20 14:11:48 +0900

    登録と更新機能の追加

--- model/riyousha.js
+++ model/riyousha.js
@@ -73,6 +73,37 @@ const findLikeForPaging = function (likevalue, percount, offset, callback) {
     })();
 };
 
+const insert = function (inObj, callback) {
+    (async function () {
+        // const client = knex.connect();
+        const query = 'insert into riyoushas values ("' + inObj.id + '","' + inObj.kubun + '","' + inObj.kubun2 + '","' + inObj.name + '",' + tool.returnvalue(inObj.kana) + ',' + tool.returnvalue(inObj.sex) + ',' + tool.returnvalue(inObj.no_yubin) + ',' + tool.returnvalue(inObj.address) + ',' + tool.returnvalue(inObj.no_tel) + ',' + tool.returnvalue(inObj.mail1) + ',' + tool.returnvalue(inObj.mail2) + ',' + tool.returnvalue(inObj.kubun_riyousha) + ',' + tool.returnvalue(inObj.kubun_vip) + ',' + tool.returnvalue(inObj.bikou) + ',' + tool.returnvalue(inObj.ymd_add) + ',' + tool.returnvalue(inObj.ymd_upd) + ')';
+        logger.info('[' + inObj.ymd_add + ']' + query);
+        await client.raw(query)
+            .then((retObj) => {
+                callback(null, retObj[0]);
+            })
+            .catch((err) => {
+                callback(err, null);
+            })
+    })();
+};
+
+
+const update = function (inObj, callback) {
+    (async function () {
+        // const client = knex.connect();
+        const query = 'update riyoushas set kubun = ' + tool.returnvalue(inObj.kubun) + ', kubun2 = ' + tool.returnvalue(inObj.kubun2) + ', name = "' + inObj.name + '", kana = ' + tool.returnvalue(inObj.kana) + ', sex = ' + tool.returnvalue(inObj.sex) + ', no_yubin = ' + tool.returnvalue(inObj.no_yubin) + ', address = ' + tool.returnvalue(inObj.address) + ', no_tel = ' + tool.returnvalue(inObj.no_tel) + ', mail1 = ' + tool.returnvalue(inObj.mail1) + ', mail2 = ' + tool.returnvalue(inObj.mail2) + ', kubun_riyousha = ' + tool.returnvalue(inObj.kubun_riyousha) + ', kubun_vip = ' + tool.returnvalue(inObj.kubun_vip) + ', bikou = ' + tool.returnvalue(inObj.bikou) + ', ymd_add = "' + inObj.ymd_add + '", ymd_upd = "' + inObj.ymd_upd + '" where id = ' + tool.returnvalue(inObj.id) + '';
+        logger.info('[' + inObj.ymd_add + ']' + query);
+        await client.raw(query)
+            .then((retObj) => {
+                callback(null, retObj[0]);
+            })
+            .catch((err) => {
+                callback(err, null);
+            })
+    })();
+};
+
 const selectSQL = function (sql, callback) {
     (async function () {
         // const client = knex.connect();
@@ -92,5 +123,7 @@ module.exports = {
     findForSelect,
     findLikeCount,
     findLikeForPaging,
+    insert,
+    update,
     selectSQL,
 };
