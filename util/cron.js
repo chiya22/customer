40de781e7b926bb65ce7df895dcd7c4fe7a1a394
chiya22 <tomoyay@gmail.com>
2021-01-20 14:13:05 +0900

    会議室利用者情報更新機能の追加

--- util/cron.js
+++ util/cron.js
@@ -4,6 +4,10 @@ const logger = log4js.configure('./config/log4js-config.json').getLogger();
 const cron = require('node-cron');
 const m_outai = require('../model/outais');
 const mail = require('./sendmail');
+const iconv = require("iconv-lite");
+const puppeteer = require("puppeteer");
+const riyousha = require('../model/riyousha');
+const fs = require("fs");
 
 // cron設定
 const startcron = () => {
@@ -47,6 +51,259 @@ const startcron = () => {
         });
     });
 
+    // 毎分実行
+    cron.schedule('0 8 * * 1-5', () => {
+        (async () => {
+            const browser = await puppeteer.launch({ headless: false });
+
+            let page = await browser.newPage();
+
+            const URL = "https://www.yamori-yoyaku.jp/studio/OfficeLogin.htm";
+            await page.goto(URL, { waitUntil: "domcontentloaded" });
+
+            // ログイン
+            await page.type('input[name="in_office"]', "");
+            await page.type('input[name="in_opassword"]', "");
+            await page.click(
+                "body > table > tbody > tr > td > table > tbody > tr:nth-child(1) > td > form > table:nth-child(2) > tbody > tr > td:nth-child(2) > input"
+            );
+
+            await page.waitFor(1000);
+            // await page.waitForNavigation({waitUntil: 'domcontentloaded'});
+
+            // 管理画面から「管理者メニュー」をクリック
+            const menu = await page.$(
+                "body > table > tbody > tr > td > table > tbody > tr > td > table:nth-child(2) > tbody > tr:nth-child(3) > td:nth-child(2) > input[type=image]:nth-child(6)"
+            );
+            await menu.click();
+
+            await page.waitFor(2000);
+
+            // 新しく開いたページを取得
+            let newPage = await getNewPage(page);
+
+            // パスワードの設定
+            await newPage.type('input[name="in_managerpassword"]', "PS10001SP");
+            const inputElement = await newPage.$("input[type=submit]");
+            await inputElement.click();
+
+            await newPage.waitFor(2000);
+
+            // 「ダウンロード」のクリック
+            await newPage.click(
+                "body > div:nth-child(3) > table > tbody > tr > th:nth-child(6) > img"
+            );
+
+            await newPage.waitFor(2000);
+
+            // 「登録車情報ダウンロード」のクリック
+            await newPage.click(
+                "#inbody > div > div:nth-child(2) > div:nth-child(1) > div.waku_5 > img"
+            );
+
+            await newPage.waitFor(2000);
+
+            // 新しく開いたページを取得
+            let newPageTouroku = await getNewPage(newPage);
+
+            // Promptが出たら必ずOKとする
+            newPageTouroku.on('dialog', async dialog => {
+                await dialog.accept();
+            });
+
+            // 開始は現在より1ヶ月前とする
+            const dt = new Date();
+            const current_y = dt.getFullYear();
+            const current_m = dt.getMonth();
+            let set_y;
+            let set_m;
+            if (current_m === 0) {
+                set_y = current_y - 1
+                set_m = 12
+            } else {
+                set_y = current_y
+                set_m = current_m + 1
+            }
+            await newPageTouroku.select('select[name="start_y"]', set_y.toString());
+            await newPageTouroku.select('select[name="start_m"]', set_m.toString());
+            // await newPageTouroku.select('select[name="end_y"]', '2020');
+            // await newPageTouroku.select('select[name="end_m"]', '12');
+
+            // 「項目名-全選択」をクリックする
+            await newPageTouroku.click(
+                "#inbody > form > table > tbody > tr:nth-child(2) > td.reserve_screen > a:nth-child(2)"
+            );
+            // 「登録者データ」をクリックする
+            await newPageTouroku.click("#inbody > form > p > input.btn_150-30");
+
+            await newPage.waitFor(2000);
+
+            // 新しく開いたページを取得
+            let newPageResult = await getNewPage(newPageTouroku);
+
+            const a_tag = await newPageResult.$('a');
+            if (a_tag) {
+                logger.info(`会議室マスタ情報をダウンロードしました：${new Date()}`);
+                await a_tag.click();
+                await page.waitFor(10000);
+            } else {
+                logger.info(`会議室マスタ情報がありませんでした：${new Date()}`);
+            }
+
+            /**
+             * 新しく開いたページを取得
+             * @param {page} page もともと開いていたページ
+             * @returns {page} 別タブで開いたページ
+             */
+            async function getNewPage(page) {
+                const pageTarget = await page.target();
+                const newTarget = await browser.waitForTarget(
+                    (target) => target.opener() === pageTarget
+                );
+                const newPage = await newTarget.page();
+                await newPage.waitForSelector("body");
+                return newPage;
+            }
+
+            await browser.close();
+
+        })();
+    })
+
+    cron.schedule('50 8 * * 1-5', () => {
+
+        const downloadfilepath = "C:\\Users\\yoshida\\Downloads";
+
+        // ダウンロードディレクトリにあるcsvファイルを取得する
+        let targetfilename = "";
+        fs.readdirSync(downloadfilepath).forEach((filename) => {
+            // *mdl.csvのファイルの場合処理をする
+            if (filename.slice(-7) === "mdl.csv") {
+                targetfilename = filename;
+                // csvファイルはShift-JISのため
+                const src = fs
+                    .createReadStream(downloadfilepath + "\\" + filename)
+                    .pipe(iconv.decodeStream("Shift_JIS"));
+                src.on("data", (chunk) => {
+                    let detaillog = "";
+                    const lines = chunk.split("\n");
+                    lines.forEach((line) => {
+                        let linecontents = line.split(",");
+                        if ((linecontents[0] !== '利用者') && (linecontents[0] !== '')) {
+
+                            logger.info(`更新前情報：${line}`);
+
+                            // linecontentsの項目
+                            // 00:利用者
+                            // 01:ふりがな
+                            // 02:生年月日
+                            // 03:性別
+                            // 04:郵便番号
+                            // 05:住所
+                            // 06:電話番号
+                            // 07:お知らせメール
+                            // 08:E-mail(1)
+                            // 09:E-mail(2)
+                            // 10:利用者区分
+                            // 11:ＶＩＰ区分
+                            // 12:施設管理番号
+                            // 13:有効期限
+                            // 14:備考
+                            // 15:登録日
+                            // 16:更新日
+
+                            // yyyy年mm月dd日　⇒　yyyymmdd
+                            const target_ymd_upd = linecontents[16].slice(0, 4) + linecontents[16].slice(5, 7) + linecontents[16].slice(8, 10)
+                            const target_ymd_add = linecontents[15].slice(0, 4) + linecontents[15].slice(5, 7) + linecontents[15].slice(8, 10)
+
+                            let inObj = {};
+                            inObj.id = linecontents[12];
+                            linecontents[0] = (linecontents[0].replace(/ /g, "　").replace(/[*]/g, "＊"));
+                            riyousha.findPKey(inObj, (err, retObj) => {
+                                if (err) { throw err };
+
+                                // 新規登録用、更新用のオブジェクトを作成
+                                inObj.id = linecontents[12];
+                                if ((linecontents[0].indexOf('●') === -1) && (linecontents[0].indexOf('■') === -1)) {
+                                    inObj.kubun = "千代田区外";
+                                    if (linecontents[0].indexOf("◆") === -1) {
+                                        inObj.kubun2 = "中小企業・公共団体";
+                                    } else {
+                                        inObj.kubun2 = "大企業・任意団体・個人・その他";
+                                    }
+                                } else {
+                                    if (linecontents[0].indexOf("■") !== -1) {
+                                        inObj.kubun = "千代田区内";
+                                    } else {
+                                        inObj.kubun = "入居者";
+                                    }
+                                    if (linecontents[0].indexOf("◆") === -1) {
+                                        inObj.kubun2 = "中小企業・公共団体・個人";
+                                    } else {
+                                        inObj.kubun2 = "大企業・任意団体";
+                                    }
+                                }
+                                inObj.name = linecontents[0];
+                                inObj.kana = linecontents[1];
+                                inObj.sex = linecontents[3];
+                                inObj.no_yubin = linecontents[4];
+                                inObj.address = linecontents[5];
+                                inObj.no_tel = linecontents[6];
+                                inObj.mail1 = linecontents[8];
+                                inObj.mail2 = linecontents[9];
+                                inObj.kubun_riyousha = linecontents[10];
+                                inObj.kubun_vip = linecontents[11];
+                                inObj.bikou = linecontents[14];
+                                inObj.ymd_add = target_ymd_add;
+                                inObj.ymd_upd = target_ymd_upd;
+
+                                // すでに利用者が存在している場合
+                                if (retObj) {
+
+                                    // 更新されているかを判別する
+                                    if ((target_ymd_add !== retObj.ymd_add) || (target_ymd_upd !== retObj.ymd_upd)) {
+
+                                        riyousha.update(inObj, (err, retObj) => {
+                                            if (err) { throw err };
+                                            if (retObj.changedRows === 0) {
+                                                logger.info(`更新対象が存在しません：${inObj.id}`);
+                                            } else {
+                                                detaillog = inObj.id + ',' + inObj.kubun + ',' + inObj.kubun2 + ',' + inObj.name + ',' + inObj.kana + ',' + inObj.sex + ',' + inObj.no_yubin + ',' + inObj.address + ',' + inObj.no_tel + ',' + inObj.mail1 + ',' + inObj.mail2 + ',' + inObj.kubun_riyousha + ',' + inObj.kubun_vip + ',' + inObj.bikou + ',' + inObj.ymd_add + ',' + inObj.ymd_upd + '\n';
+                                                logger.info(`会議室利用者情報更新ログ：${detaillog}`);
+                                            }
+                                        })
+                                    }
+
+                                    // 利用者に存在しない場合
+                                } else {
+
+                                    riyousha.insert(inObj, (err, retObj) => {
+                                        if (err) { throw err };
+                                        detaillog = inObj.id + ',' + inObj.kubun + ',' + inObj.kubun2 + ',' + inObj.name + ',' + inObj.kana + ',' + inObj.sex + ',' + inObj.no_yubin + ',' + inObj.address + ',' + inObj.no_tel + ',' + inObj.mail1 + ',' + inObj.mail2 + ',' + inObj.kubun_riyousha + ',' + inObj.kubun_vip + ',' + inObj.bikou + ',' + inObj.ymd_add + ',' + inObj.ymd_upd + '\n';
+                                        logger.info(`会議室利用者情報登録ログ：${detaillog}`);
+                                    })
+                                }
+                            });
+                        }
+                    });
+                });
+
+                src.on("end", () => {
+                    // 対象ファイルを処理した場合は対象ファイルをリネーム
+                    fs.rename(
+                        downloadfilepath + "\\" + targetfilename,
+                        downloadfilepath + "\\" + targetfilename + ".old",
+                        (err) => {
+                            if (err) {
+                                logger.info(`${targetfilename}ファイルは存在しません：${new Date()}`);
+                                throw err;
+                            }
+                        }
+                    );
+                });
+            }
+        });
+    });
 }
 
 module.exports = {
